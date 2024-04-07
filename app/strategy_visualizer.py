51b7ae04183bcf1adc29f0797a3d5afdd6094290
proman3419 <proman7268@gmail.com>
2024-04-07 18:49:08 +0200

    Added strategy performance relative to the optimal one

--- app/strategy_visualizer.py
+++ app/strategy_visualizer.py
@@ -23,17 +23,18 @@ def print_operation(date, action: Action, close, money, shares):
 
 
 def print_strategy(stock_data, ta_features, strategy, initial_money=1000):
+    dates = stock_data['date'].to_numpy()
     money = initial_money
     shares = 0
     for i in range(len(ta_features) - 1):
         if np.sum(ta_features.iloc[i].to_numpy() * np.array(strategy)) > 0 and money > 0:
             shares = money / stock_data.iloc[i]['close']
             money = 0
-            print_operation(stock_data.iloc[i]['date'], Action.BUY, stock_data.iloc[i]['close'], money, shares)
+            print_operation(dates[i], Action.BUY, stock_data.iloc[i]['close'], money, shares)
         elif np.sum(ta_features.iloc[i].to_numpy() * np.array(strategy)) < 0 and shares > 0:
             money = shares * stock_data.iloc[i]['close']
             shares = 0
-            print_operation(stock_data.iloc[i]['date'], Action.SELL, stock_data.iloc[i]['close'], money, shares)
+            print_operation(dates[i], Action.SELL, stock_data.iloc[i]['close'], money, shares)
     if shares > 0:
         money = shares * stock_data.iloc[-1]['close']
         shares = 0
@@ -45,53 +46,88 @@ def print_strategy(stock_data, ta_features, strategy, initial_money=1000):
     )
 
 
-def plot_strategy(stock_data, ta_features, strategy, initial_money=1000):
-    x = stock_data['date'].to_numpy()
+def simulate_strategy(stock_data, ta_features, strategy, initial_money=1000):
+    dates = stock_data['date'].to_numpy()
+    close_prices = stock_data['close'].to_numpy()
     x_buy = []
     y_buy = []
     x_sell = []
     y_sell = []
-    y_close = stock_data['close'].to_numpy()
-    y_sum_money_and_money_in_shares = []
+    sum_money_and_money_in_shares = []
     money = initial_money
     shares = 0
     for i in range(len(ta_features) - 1):
         if np.sum(ta_features.iloc[i].to_numpy() * np.array(strategy)) > 0 and money > 0:
-            shares = money / stock_data.iloc[i]['close']
+            shares = money / close_prices[i]
             money = 0
-            x_buy.append(stock_data.iloc[i]['date'])
-            y_buy.append(stock_data.iloc[i]['close'])
+            x_buy.append(dates[i])
+            y_buy.append(close_prices[i])
         elif np.sum(ta_features.iloc[i].to_numpy() * np.array(strategy)) < 0 and shares > 0:
-            money = shares * stock_data.iloc[i]['close']
+            money = shares * close_prices[i]
             shares = 0
-            x_sell.append(stock_data.iloc[i]['date'])
-            y_sell.append(stock_data.iloc[i]['close'])
-        y_sum_money_and_money_in_shares.append(money + shares * stock_data.iloc[i]['close'])
+            x_sell.append(dates[i])
+            y_sell.append(close_prices[i])
+        sum_money_and_money_in_shares.append(money + shares * close_prices[i])
     if shares > 0:
-        money = shares * stock_data.iloc[-1]['close']
-        y_sum_money_and_money_in_shares.append(money)
-        x_sell.append(stock_data.iloc[-1]['date'])
-        y_sell.append(stock_data.iloc[-1]['close'])
+        money = shares * close_prices[-1]
+        x_sell.append(dates[-1])
+        y_sell.append(close_prices[-1])
+    sum_money_and_money_in_shares.append(money)
+    return dates, close_prices, x_buy, y_buy, x_sell, y_sell, sum_money_and_money_in_shares
+
+
+def simulate_best_possible_strategy(stock_data, initial_money=1000):
+    dates = stock_data['date'].to_numpy()
+    close_prices = stock_data['close'].to_numpy()
+    x_buy = []
+    y_buy = []
+    x_sell = []
+    y_sell = []
+    sum_money_and_money_in_shares = []
+    money = initial_money
+    shares = 0
+    for i in range(len(dates) - 1):
+        if stock_data.iloc[i]['close'] < stock_data.iloc[i+1]['close'] and money > 0:
+            shares = money / close_prices[i]
+            money = 0
+            x_buy.append(dates[i])
+            y_buy.append(close_prices[i])
+        elif stock_data.iloc[i]['close'] > stock_data.iloc[i+1]['close'] and shares > 0:
+            money = shares * close_prices[i]
+            shares = 0
+            x_sell.append(dates[i])
+            y_sell.append(close_prices[i])
+        sum_money_and_money_in_shares.append(money + shares * close_prices[i])
+    if shares > 0:
+        money = shares * close_prices[-1]
+        x_sell.append(dates[-1])
+        y_sell.append(close_prices[-1])
+    sum_money_and_money_in_shares.append(money)
+    return dates, close_prices, x_buy, y_buy, x_sell, y_sell, sum_money_and_money_in_shares
+
+
+def plot_strategy(dates, close_prices, x_buy, y_buy, x_sell, y_sell, sum_money_and_money_in_shares):
+    dates_cnt = len(dates)
 
     plt.style.use('bmh')
     plt.figure(figsize=(16, 18), dpi=400)
     plt.subplot(211)
-    plt.plot(x, y_close, linewidth=0.75)
+    plt.plot(dates, close_prices, linewidth=0.75)
     plt.plot(x_buy, y_buy, 'g^', label=f'Buy')
     plt.plot(x_sell, y_sell, 'rv', label=f'Sell')
     plt.xlabel('Date')
     plt.ylabel('Close price')
     plt.title('Strategy visualization')
     plt.grid(True)
-    plt.xticks(np.arange(0, len(x) - 1, len(x) // 20), rotation=45)
+    plt.xticks(np.arange(0, dates_cnt - 1, dates_cnt // 20), rotation=45)
     plt.legend(loc='upper left')
 
     plt.subplot(212)
-    plt.plot(x, y_sum_money_and_money_in_shares, color='orange', linewidth=0.75)
+    plt.plot(dates, sum_money_and_money_in_shares, color='orange', linewidth=0.75)
     plt.xlabel('Date')
     plt.ylabel('Money')
     plt.title('Money + money in shares')
     plt.grid(True)
-    plt.xticks(np.arange(0, len(x) - 1, len(x) // 20), rotation=45)
+    plt.xticks(np.arange(0, dates_cnt - 1, dates_cnt // 20), rotation=45)
 
     plt.show()
