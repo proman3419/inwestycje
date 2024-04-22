f8b08deb6e568bb36bcc25271337d3e2e5630b29
proman3419 <proman7268@gmail.com>
2024-04-22 14:27:53 +0200

    Moved to preprocessing, added merging with ta_features

--- app/preprocessing.py
+++ app/preprocessing.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+import inspect
+import random
 import ta
 
 from sklearn.preprocessing import MinMaxScaler
@@ -9,6 +11,11 @@ from ta.utils import dropna
 def preprocess_data(path: str) -> (pd.DataFrame, pd.DataFrame, pd.DataFrame):
     stock_data, ta_features, preview = load_data(path)
     drop_highly_correlated(ta_features)
+    generated_features = generate_features()
+    ta_features = append_features(generated_features, ta_features)
+    ta_features = ta_features.tail(-400)
+    stock_data = stock_data.tail(-400)
+    preview = preview.tail(-400)
     return stock_data, ta_features, preview
 
 
@@ -27,6 +34,88 @@ def load_data(path: str) -> (pd.DataFrame, pd.DataFrame, pd.DataFrame):
     ta_features = df.iloc[:, 6:]
     preview = pd.concat([stock_data, ta_features],axis=1)
     return stock_data, ta_features, preview
+
+
+def generate_features():
+    random.seed(420)
+    stock_data, ta_features, preview = load_data("./data/wig_d.csv")
+    constructors = get_constructors(ta.momentum)
+    instances = inject_params(constructors, stock_data)
+    features = retrieve_features(instances)
+    return features
+
+
+def append_features(features, ta_features):
+    series = []
+    for feature_name, feature_vals in features.items():
+        series.append(pd.Series(feature_vals, name=feature_name))
+    df = pd.concat(series, axis=1)
+    df = df.tail(-365)
+    df = pd.concat([ta_features, df], axis=1)
+    return df
+
+
+def get_constructors(module):
+    # Get all class names defined in the module
+    class_names = [name for name in dir(module) if isinstance(getattr(module, name), type)]
+    constructors = []
+    # Print constructors for each class with their arguments
+    for class_name in class_names:
+        class_ = getattr(module, class_name)
+        # Check if it's a class (not a module or function)
+        if isinstance(class_, type):
+            # Check if the class has a __init__ method
+            if hasattr(class_, '__init__'):
+                # Get the constructor signature
+                init_signature = inspect.signature(getattr(class_, '__init__'))
+                full_class_name = f'{module.__name__}.{class_name}'
+                constructor = (class_, init_signature)
+                constructors.append(constructor)
+                print(constructor)
+            else:
+                # print(f"Class {class_name} does not have a constructor (__init__ method)")
+                pass
+    return constructors
+
+
+def inject_params(constructors, stock_data):
+    instances = dict()
+    for constructor in constructors:
+        init_signature = constructor[1]
+        to_inject = dict()
+        for param in init_signature.parameters.values():
+            if param.name in ('self', 'args', 'kwargs'):
+                continue
+            if param.name in stock_data:
+                to_inject[param.name] = stock_data[param.name]
+            else:
+                _type = type(param.default)
+                value = None
+                if _type is int:
+                    value = random.randint(0, 100)
+                if _type is float:
+                    value = random.uniform(0, 100)
+                if _type is bool:
+                    value = False
+                to_inject[param.name] = value
+        instance = constructor[0]() if len(init_signature.parameters.values()) == 1 else constructor[0](**to_inject)
+        instances['GENERATED_' + constructor[0].__name__] = instance
+    return instances
+
+
+def retrieve_features(instances):
+    features = dict()
+    for feature_name, instance in instances.items():
+        method_names = [m for m in dir(instance) if m[0] != '_']
+        for method_name in method_names:
+            method = getattr(instance, method_name)
+            feature_vals = method()
+            print(feature_name, feature_vals.isna().sum())
+            if feature_vals.isna().sum() < 365:
+                features[feature_name] = feature_vals
+        if len(method_names) == 0:
+            continue
+    return features
 
 
 # https://stackoverflow.com/questions/29294983/how-to-calculate-correlation-between-all-columns-and-remove-highly-correlated-on
